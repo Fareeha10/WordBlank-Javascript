308f778f7ccdd6a660e30c499849ab16a65a351d
Fareeha10 <42777932+Fareeha10@users.noreply.github.com>
2023-06-15 22:03:38 +0500

    Add files via upload

---
+++ WordBlank.js
@@ -0,0 +1,6 @@
+function WordBlank(noun, adj,verb, adv)
+{
+    var result = "";
+    result +="The "+adj+" "+noun+" "+verb+" "+"to the store."
+}
+console.log(WordBlank("dog","big", "ran","quickly"));
